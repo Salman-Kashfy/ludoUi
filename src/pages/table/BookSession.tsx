01d0b9ff75f56af76805a81e558037ae68abeac6
Salman Kashfy <salmankashfy@gmail.com>
2025-10-23 20:50:24 +0500

    Add filter options on customer search

--- src/pages/table/BookSession.tsx
+++ src/pages/table/BookSession.tsx
@@ -1,7 +1,7 @@
 import Grid from '@mui/material/Grid';
 import { Controller, useForm } from 'react-hook-form';
 import FormInput from '../../components/FormInput';
-import { Fragment, useState, useEffect } from 'react';
+import { Fragment, useState, useEffect, useCallback, useContext } from 'react';
 import { CircularProgress, Button } from '@mui/material';
 import { Autocomplete } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
@@ -13,6 +13,9 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import {debounce} from "@mui/material/utils";
+import { GetCustomers } from '../../services/customer.service';
+import {CompanyContext} from '../../hooks/CompanyContext';
 
 const BookSession = ({open, handleDialogClose, tableUuid, onSuccess}:{open:boolean, handleDialogClose:() => void, tableUuid:string, onSuccess:() => void}) => {
 
@@ -26,11 +29,44 @@ const BookSession = ({open, handleDialogClose, tableUuid, onSuccess}:{open:boole
         defaultValues
     })
     const [bookSessionLoader, setBookSessionLoader] = useState(false);
+    const companyContext:any = useContext(CompanyContext)
+    const companyUuid = companyContext.companyUuid
 
     const [customers, setCustomers] = useState<any[]>([]);
     const [customerId, setCustomerId] = useState<{label: string, value: string}>({label: '', value: ''});
-    const [customerLoader, setCustomerLoader] = useState<boolean>(false);
-    const [searchCustomer, setSearchCustomer] = useState<string>('');
+    const [customerLoader, setCustomerLoader] = useState(false);
+    const [searchCustomer, setSearchCustomer] = useState("");
+
+    const fetchCustomers = ({searchCustomer, companyUuid}:{searchCustomer:string, companyUuid:string}) => {
+        setCustomerLoader(true)
+        GetCustomers({}, {companyUuid, searchText: searchCustomer}).then((response:any) => {
+            const { list } = response
+            const rows = list.map((e:any) => {
+                return {
+                    value: e.uuid,
+                    label: e.fullName+' ('+e.phone+')',
+                    phoneCode: e.fullName+' ('+e.phoneCode+')',
+                }
+            })
+            console.log({rows})
+            setCustomers(() => rows)
+        }).catch((e) => {
+            console.log(e.message)
+        }).finally(() => {
+            setCustomerLoader(false)
+        })
+    }
+
+    const debouncedFetchCustomer = useCallback(
+        debounce((searchCustomer, companyUuid) => fetchCustomers({searchCustomer, companyUuid}), 800),
+        []
+    );
+
+    useEffect(() => {
+        if (searchCustomer){
+            debouncedFetchCustomer(searchCustomer, companyUuid)
+        }
+    }, [searchCustomer, debouncedFetchCustomer]);
 
     useEffect(() => {
         reset(defaultValues)
@@ -77,7 +113,7 @@ const BookSession = ({open, handleDialogClose, tableUuid, onSuccess}:{open:boole
                                             message: "Status is required"
                                         },
                                     }}
-                                    render={({ field, fieldState: { error } }) => (
+                                    render={() => (
                                         <Autocomplete
                                             id="customer-dd"
                                             options={customers}
@@ -86,6 +122,15 @@ const BookSession = ({open, handleDialogClose, tableUuid, onSuccess}:{open:boole
                                             onInput={(event:any) => setSearchCustomer(event.target.value)}
                                             getOptionLabel={(option) => option.label || ''}
                                             onChange={handleCustomerChange}
+                                            filterOptions={(options, state) => {
+                                                const input = state.inputValue.trim();
+                                                return options.filter(
+                                                  (option) =>
+                                                    option.label.toLowerCase().includes(input.toLowerCase()) ||
+                                                    option.label.includes(input.replace(/^0/, option.phoneCode)) ||
+                                                    option.label.includes(input.replace(/^0/, ''))
+                                                );
+                                            }}
                                             renderInput={(params) => <FormInput fullWidth={true} label={'Customer'} placeholder={'Search by name or phone number'} params={params}
                                                 slotProps={{
                                                     input: {
